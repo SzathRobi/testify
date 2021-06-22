5f24cbf7e5302c330f5a52f56fc67f97bd91a364
DevVatu <devvatu@gmail.com>
2021-06-22 11:32:40 +0200

    maybe fixed prisma bug

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,13 +2,13 @@ import { NextApiHandler } from "next";
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 import Adapters from "next-auth/adapters";
-import prisma from '../../../lib/prisma'
-
+import prisma from "../../../lib/prisma";
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
 export default authHandler;
 
 const options = {
+  site: process.env.NEXTAUTH_URL || "http://localhost:3000",
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
